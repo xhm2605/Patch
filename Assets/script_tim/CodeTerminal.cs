6176c9661240583c39d69748117fae911ccf8755
timothe3833 <vcp3833@autuni.ac.nz>
2026-09-09 01:51:03 +1200

    code terminal

--- Assets/script_tim/CodeTerminal.cs
+++ Assets/script_tim/CodeTerminal.cs
@@ -5,27 +5,26 @@ using TMPro;
 public class CodeTerminal : MonoBehaviour
 {
     [Header("Références UI")]
-    public Button codeButton;
-    public GameObject codePanel;
-    public TMP_Text feedbackText;
+    public Button CodeButton;
+    public GameObject CodePanel;
+    public TMP_Text FeedbackText;
 
     private bool playerInRange = false;
 
     void Start()
     {
-        if (codeButton != null) codeButton.gameObject.SetActive(false);
-        if (codePanel != null) codePanel.SetActive(false);
+        if (CodeButton != null) CodeButton.gameObject.SetActive(false);
+        if (CodePanel != null) CodePanel.SetActive(false);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Quelque chose est entré : " + other.name);
         if (!other.CompareTag("Player")) return;
 
         playerInRange = true;
-        codeButton.gameObject.SetActive(true);
-        codeButton.onClick.RemoveAllListeners();
-        codeButton.onClick.AddListener(TryOpenCodePanel);
+        CodeButton.gameObject.SetActive(true);
+        CodeButton.onClick.RemoveAllListeners();
+        CodeButton.onClick.AddListener(TryOpenCodePanel);
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -33,8 +32,8 @@ public class CodeTerminal : MonoBehaviour
         if (!other.CompareTag("Player")) return;
 
         playerInRange = false;
-        codeButton.onClick.RemoveAllListeners();
-        codeButton.gameObject.SetActive(false);
+        CodeButton.onClick.RemoveAllListeners();
+        CodeButton.gameObject.SetActive(false);
     }
 
     void TryOpenCodePanel()
@@ -47,12 +46,12 @@ public class CodeTerminal : MonoBehaviour
         {
             // Story 8 : accès refusé tant que tout n'est pas réparé
             Debug.Log("Accès refusé : " + repaired + "/4 systèmes réparés");
-            if (feedbackText != null)
-                feedbackText.text = "REPAIRS INCOMPLETE : " + repaired + "/4";
+            if (FeedbackText != null)
+                FeedbackText.text = "REPAIRS INCOMPLETE : " + repaired + "/4";
             return;
         }
 
-        codeButton.gameObject.SetActive(false);
-        codePanel.SetActive(true);
+        CodeButton.gameObject.SetActive(false);
+        CodePanel.SetActive(true);
     }
 }
