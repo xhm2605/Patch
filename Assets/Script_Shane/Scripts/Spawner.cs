acf21ddc40ad59228a18b3da23579c9d5508193c
lambo6767 <shanelam623@gmail.com>
2026-09-17 13:48:55 +1200

    upload complete

---
+++ Assets/Script_Shane/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float minTime = 2f;
+    public float maxTime = 4f;
+    public void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    public void Spawn()
+    {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+        Invoke(nameof(Spawn), Random.Range(minTime, maxTime));
+    }
+}
